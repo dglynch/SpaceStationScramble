cd6215afbf2bbe55821f0eb8b413c4bda127e59f
Dan Lynch <dglynch@dglynch.com>
2015-01-24 00:21:13 -0500

    Fix code style formatting

--- SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/Program.cs
+++ SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/Program.cs
@@ -2,19 +2,15 @@ using System;
 
 namespace SpaceStationScramble {
 #if WINDOWS || XBOX
-    static class Program
-    {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main(string[] args)
-        {
-            using (SpaceStationScrambleGame game = new SpaceStationScrambleGame())
-            {
+        static void Main(string[] args) {
+            using (SpaceStationScrambleGame game = new SpaceStationScrambleGame()) {
                 game.Run();
             }
         }
     }
 #endif
 }
-
