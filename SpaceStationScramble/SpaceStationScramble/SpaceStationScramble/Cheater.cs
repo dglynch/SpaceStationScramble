91eaee3df0b69c6b0356be6e53bc6693d3ef5c55
Alex Allin <allin.alexander@gmail.com>
2015-01-24 12:12:24 -0500

    Add cheat mode constant

---
+++ SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/Cheater.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStationScramble {
+    class Cheater {
+        public const bool CheatsOn = false;
+    }
+}
