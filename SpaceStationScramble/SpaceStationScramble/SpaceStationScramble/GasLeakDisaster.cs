6b4f11391fb6f83eca825dbf7062c4a7d67ea472
Alex Allin <allin.alexander@gmail.com>
2015-01-25 14:39:39 -0500

    Altered gas leak graphics and positioning

--- SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/GasLeakDisaster.cs
+++ SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/GasLeakDisaster.cs
@@ -7,11 +7,11 @@ using System.Text;
 
 namespace SpaceStationScramble {
     class GasLeakDisaster : DisasterEvent {
-        private readonly Vector2 northDrawLoc = new Vector2(640, 60);
-        private readonly Vector2 southDrawLoc = new Vector2(640, 660);
-        private readonly Vector2 eastDrawLoc = new Vector2(980, 360);
-        private readonly Vector2 westDrawLoc = new Vector2(300, 360);
-        private readonly Vector2 centerDrawLoc = new Vector2(640, 360);
+        private readonly Vector2 northDrawLoc = new Vector2(640, 280);
+        private readonly Vector2 southDrawLoc = new Vector2(640, 570);
+        private readonly Vector2 eastDrawLoc = new Vector2(800, 420);
+        private readonly Vector2 westDrawLoc = new Vector2(475, 420);
+        private readonly Vector2 centerDrawLoc = new Vector2(640, 420);
 
         public SteamColor SteamColor {
             get;
@@ -43,18 +43,20 @@ namespace SpaceStationScramble {
             }
         }
         public override void Draw(SpriteBatch spriteBatch) {
+            Vector2 textureOffset = new Vector2(-SpaceStationScrambleGame.steamTexture.Width / 2,
+                -SpaceStationScrambleGame.steamTexture.Height / 2);
             switch (SteamColor) {
                 case SteamColor.Red:
-                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position, Color.Red);
+                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position + textureOffset, Color.Red);
                     break;
                 case SteamColor.Blue:
-                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position, Color.Blue);
+                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position + textureOffset, Color.Blue);
                     break;
                 case SteamColor.Green:
-                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position, Color.Green);
+                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position + textureOffset, Color.DarkGreen);
                     break;
                 case SteamColor.Yellow:
-                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position, Color.Yellow);
+                    spriteBatch.Draw(SpaceStationScrambleGame.steamTexture, Position + textureOffset, Color.Yellow);
                     break;
             }
         }
