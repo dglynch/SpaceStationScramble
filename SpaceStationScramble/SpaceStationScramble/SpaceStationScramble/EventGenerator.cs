40fdf907acaf62008b69111a8a041577bb88c59e
Dan Lynch <dglynch@dglynch.com>
2015-01-24 23:00:00 -0500

    First serious attempt at balancing for fun

--- SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/EventGenerator.cs
+++ SpaceStationScramble/SpaceStationScramble/SpaceStationScramble/EventGenerator.cs
@@ -13,14 +13,16 @@ namespace SpaceStationScramble {
         private int maxEventInterval;
         private int minDuration;
         private int maxDuration;
+        private long lastEventStarted;
 
         public EventGenerator(Synchronizer synchronizer) {
             this.synchronizer = synchronizer;
             //Eventually these times will change as time goes on
-            minEventInterval = 12000;
-            maxEventInterval = 17000;
+            minEventInterval = 6000;
+            maxEventInterval = 10000;
             minDuration = 10000;
-            maxDuration = 27000;
+            maxDuration = 14000;
+            lastEventStarted = 0;
 
             eventSlots = new Dictionary<EventSlot, long>();
             foreach(EventSlot slot in Enum.GetValues(typeof (EventSlot))) {
@@ -30,12 +32,37 @@ namespace SpaceStationScramble {
 
         public DisasterEvent NextEvent() {
             //Next event time is last event time plus random value
-            var slots = Enum.GetValues(typeof(EventSlot));
-            EventSlot nextSlot = (EventSlot)(slots.GetValue(synchronizer.Next(0, slots.Length)));
+            long nextEventTime;
+            if (lastEventStarted == 0) {
+                nextEventTime = synchronizer.Next(3000, 5000);
+            } else {
+                nextEventTime = lastEventStarted + synchronizer.Next(minEventInterval, maxEventInterval);
+            }
 
-            long nextEventTime = eventSlots[nextSlot] + synchronizer.Next(minEventInterval, maxEventInterval);
-            long eventDuration = synchronizer.Next(minDuration, maxDuration); //TODO random durations?
+            var slots = Enum.GetValues(typeof(EventSlot)).Cast<EventSlot>().ToList();
+            List<EventSlot> goodEvents = new List<EventSlot>();
+            foreach (var slot in slots) {
+                if (eventSlots[slot] < nextEventTime) {
+                    goodEvents.Add(slot);
+                }
+            }
+
+            EventSlot nextSlot;
+            if (goodEvents.Count > 0) {
+                nextSlot = goodEvents[synchronizer.Next(0, goodEvents.Count)];
+            } else {
+                nextEventTime += 10000;
+                foreach (var slot in slots) {
+                    if (eventSlots[slot] < nextEventTime) {
+                        goodEvents.Add(slot);
+                    }
+                }
+                nextSlot = goodEvents[synchronizer.Next(0, slots.Count)];
+            }
+
+            long eventDuration = synchronizer.Next(minDuration, maxDuration);
             eventSlots[nextSlot] = nextEventTime + eventDuration;
+            lastEventStarted = nextEventTime;
 
             //Currently only creating gas leaks
             var steamColors = Enum.GetValues(typeof(SteamColor));
